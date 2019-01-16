c493bb34cfb0d66d832f0c4267de808c1f4b4ffe
rosilioroman <rosilioroman@gmail.com>
2019-01-16 14:23:11 -0700

    Created the beginnings of the TodoForm component; added a text input field that takes in a todoList array as a prop.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -6,12 +7,15 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      todoList: []
+    };
   }
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <h1>Ro's Todo App</h1>
+        <TodoForm currentList={this.state.todoList}/>
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoForm = props => {
+    return (
+        <div className="todo-form">
+            <input type="text" placeholder="What do you need to do?"></input>
+        </div>
+    );
+}
+
+export default TodoForm;
