cf1b89d861e55365d1f27d556528466a6f8450e0
rosilioroman <rosilioroman@gmail.com>
2019-01-17 12:47:50 -0700

    Added line-through style to completed list items (check comments in Todo.js)

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 // TodoItem is a functional component that receives an object from the todoList array found in the app's state
 // It returns a div element with a child element containing the itemName of the object passed down by TodoList
-// The div element has a className called "todo-list-iem" and a click event handler that calls a function passed down from App.js
+// The div element has a className called "todo-list-item" and a click event handler that calls a function passed down from App.js
+// The div element also has a style attribute that is toggled on/off based on the value of the object's completed property.
 const TodoItem = props => {
     return (
-        <div className="todo-list-item" onClick={() => props.completedHandler(props.todoItem.id)}> 
+        <div className="todo-list-item" onClick={() => props.completedHandler(props.todoItem.id)} style={props.todoItem.completed ? {textDecoration: 'line-through'} : null}> 
             {props.todoItem.itemName}
         </div>
     );
